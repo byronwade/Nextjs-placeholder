f19b665748276c8a217f5b0feccd4990395aebdb
Byron Wade <bcw1995@gmail.com>
2022-12-30 00:59:31 -0800

    more

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,11 @@ import { useLayoutEffect } from 'react';
 import {ThemeProvider, BaseStyles} from '@primer/react'
 import {themeGet} from '@primer/react'
 import styled from 'styled-components'
+import '../styles/globals.scss'
+import { DefaultSeo, BrandJsonLd, WebPageJsonLd } from 'next-seo';
+
+// import your default seo configuration
+import SEO from '../next-seo.config';
 
 //to suppress all the warings from useLayoutEffect
 if (typeof window === "undefined") useLayoutEffect = () => {};
@@ -15,6 +20,25 @@ const Background = styled.div`
 export default function App({ Component, pageProps }) {
 	return (
 		<ThemeProvider colorMode="auto" preventSSRMismatch>
+        	<DefaultSeo {...SEO} />
+			<BrandJsonLd
+				slogan="What does the fox say?"
+				id="https://www.purpule-fox.io/#corporation"
+				logo="https://www.example.com/photos/logo.jpg"
+				aggregateRating={{
+					ratingValue: '5',
+					ratingCount: '18',
+				}}
+			/>
+			<WebPageJsonLd
+				description="What does the fox say?"
+				id="https://www.purpule-fox.io/#corporation"
+				lastReviewed="2021-05-26T05:59:02.085Z"
+				reviewedBy={{
+					type: 'Person',
+					name: 'Garmeeh',
+				}}
+			/>
 			<BaseStyles>
 				<Background>
 					<Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,21 @@
 //import React, { useState } from 'react';
-import {Button} from '@primer/react'
-
+import Link from "next/link";
+import PageContent from "../components/pageContent";
 import DarkMode from '../components/darkMode'
 
 
 export default function Home() {
 	return (
-		<>
-			<DarkMode />
-			<div>
-				<p className="h0-mktg">This is heading 0</p>
-				<Button>Home</Button>
-			</div>
-		</>
+		<PageContent>
+			<section className="hero">
+				<div className="container">
+					<h1>Website Under Construction</h1>
+					<p>whatsdns.org is coming soon. If you would like to contribute to this website please contact me at <Link href="#">bw@wadesinc.io</Link>.</p>
+					<p>This project will be opensource and on my <Link href="https://github.com/byronwade">github</Link></p>
+					<p>Presented By <Link href="https://www.byronwade.com/">Wade's Website Development</Link> a <Link href="https://www.byronwade.com/">Wade's Inc.</Link> company.</p>
+					<p>Change the color: <DarkMode /></p>
+				</div>
+			</section>
+		</PageContent>
 	);
 }
